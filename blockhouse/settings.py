457c2ebcf029b14a0df1cca4756e653350bc0e8c
muyangli2855 <ml2855@cornell.edu>
2024-10-20 16:31:05 -0400

    1

--- blockhouse/settings.py
+++ blockhouse/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gtjf$yjp*hw0v=25ow$dsa)&cpfo*yr$#)81$ox_l6vewn$^z1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.137.195.47', 'localhost', '127.0.0.1']
 
 
 # Application definition
